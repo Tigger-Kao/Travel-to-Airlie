00a7aed708b30eb507a93b574351c103803a9112
Tigger <tea411139@hotmail.com>
2019-12-24 18:31:06 +0800

    1224

--- nav.js
+++ nav.js
@@ -1,23 +1,22 @@
-$("button").on("click", function() {
+$(".menu-toggle").on("click", function() {
   $("body").toggleClass("open");
 });
 
-$(document).ready(function() {
-  $(".toggle_group").each(function() {
-    var $dropdown = $(this);
+$(".toggle_group").each(function() {
+  var $dropdown = $(this);
 
-    $(".hotSpot_toggle_button", $dropdown).click(function(e) {
-      e.preventDefault();
-      $div = $(".hotSpot_toggle", $dropdown);
-      $div.toggle(300);
-      $(".hotSpot_toggle")
-        .not($div)
-        .hide();
-      return false;
-    });
-  });
-
-  $("html").click(function() {
-    $(".hotSpot_toggle").hide();
+  $(".hotSpot_toggle_button", $dropdown).click(function(e) {
+    e.preventDefault();
+    $div = $(".hotSpot_toggle", $dropdown);
+    $div.toggle(300);
+    $(".hotSpot_toggle")
+      .not($div)
+      .hide();
+    return false;
   });
 });
+
+$("html").click(function() {
+  $(".hotSpot_toggle").hide();
+});
+
